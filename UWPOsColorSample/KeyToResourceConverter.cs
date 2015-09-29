67139192bcc8b0d578aec98f1e945d06a70ae956
matatabi-ux <matatabi-ux@live.jp>
2015-09-29 21:15:20 +0900

    first commit

---
+++ UWPOsColorSample/KeyToResourceConverter.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Data;
+
+namespace UWPOsColorSample
+{
+    /// <summary>
+    /// Resource key string to resource value converter
+    /// </summary>
+    public class KeyToResourceConverter : IValueConverter
+    {
+        /// <summary>
+        /// ResourceKey to resource value convert
+        /// </summary>
+        /// <param name="value">ResourceKey string</param>
+        /// <param name="targetType">target type</param>
+        /// <param name="parameter">paraneter</param>
+        /// <param name="language">culture</param>
+        /// <returns>resource value</returns>
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if (value == null || !App.Current.Resources.ContainsKey(value.ToString()))
+            {
+                return DependencyProperty.UnsetValue;
+            }
+
+            return App.Current.Resources[value.ToString()];
+        }
+
+        /// <summary>
+        /// Resource value to ResourceKey convert
+        /// </summary>
+        /// <param name="value">resource value</param>
+        /// <param name="targetType">target type</param>
+        /// <param name="parameter">paraneter</param>
+        /// <param name="language">culture</param>
+        /// <returns>ResourceKey string</returns>
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
---
+++ UWPOsColorSample/MainPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace UWPOsColorSample
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        /// <summary>
+        /// Open settings button click event handler
+        /// </summary>
+        /// <param name="sender">event sender</param>
+        /// <param name="e">event arguments</param>
+        private async void OnOpenSettingsButtonClick(object sender, RoutedEventArgs e)
+        {
+            await Launcher.LaunchUriAsync(new Uri("ms-settings:personalization-colors"));
+        }
+    }
+}
